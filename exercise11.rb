33ebb8193737a365e63be57c32b69170a703e29f
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-01-10 12:43:18 +0530

    [10 Jan] [Ex 11] Implementing a method that returns the common elements between two arrays

---
+++ exercise11.rb
@@ -0,0 +1,17 @@
+def common_elements(arr1, arr2)
+  common_elements_arr = []
+
+  arr1.each_with_index do |element, index|
+    common_elements_arr << element if arr2.include?(element)
+  end
+
+ return common_elements_arr
+end
+
+
+array1 = [1, 2, 3, 4, 5]
+array2 = [2, 3, 4, 5, 6, 7]
+
+result = common_elements(array1, array2)
+
+puts "Common Elements: #{result}"
