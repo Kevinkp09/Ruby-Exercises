0a57bdad7f8afa3015a78941b18952229d257f0c
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-01-10 11:32:53 +0530

    [10 Jan] [Ex 7] Implementing a methodd to calculate the sum of elements in array

---
+++ exercise7.rb
@@ -0,0 +1,17 @@
+# 7.Implement a method to calculate the sum of all elements in an array.
+
+def calculate_sum()
+  sum = 0
+  arr = [1, 2, 3, 4, 5]
+  arr.each do |i|
+    sum += i
+  end
+
+  return sum
+end
+
+
+
+result = calculate_sum()
+
+puts "Sum of elements: #{result}"
