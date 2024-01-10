fc9cd08554956c1dda477a28a221afad188ed81c
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-01-10 11:05:48 +0530

    [10 Jan] [Ex 3] Implementing a method that capitalizes the first letter of each word in string

---
+++ exercise3.rb
@@ -0,0 +1,13 @@
+# 3.Create a method that capitalizes the first letter of each word in a string.
+
+def capitalizes(str)
+
+  str_split = str.split
+  str_capitalized = str_split.map{|word| word.capitalize()}
+  puts str_capitalized.join(" ")
+
+end
+
+string = "all is well"
+result = capitalizes(string)
+puts result
