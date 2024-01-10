ee42d8242fcb9d3a858cb646302f74c1da6f0f63
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-01-10 11:44:25 +0530

    [10 Jan] [Ex 10] Implementing a function that checks if two arrays are equal

---
+++ exercise10.rb
@@ -0,0 +1,24 @@
+# 10.Implement a function that checks if two arrays are equal (contain the same elements in the same order).
+
+def arrays_equal(arr1, arr2)
+
+  return false unless arr1.length == arr2.length
+
+
+  arr1.each_with_index do |element, i|
+    return false unless element == arr2[i]
+  end
+
+
+  true
+end
+
+
+arr1 = [1, 2, 3, 4, 5]
+arr2 = [1, 2, 3, 4, 5]
+arr3 = [1, 2, 3, 4, 6]
+arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+puts arrays_equal(arr1, arr2)
+puts arrays_equal(arr1, arr3)
+puts arrays_equal(arr2, arr4)
