ce8b29deecdc012ff0baded845ddb7fd7852f20e
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-01-11 11:12:59 +0530

    Made the required changes

--- exercise3.rb
+++ exercise3.rb
@@ -1,10 +1,9 @@
 # 3.Create a method that capitalizes the first letter of each word in a string.
 
 def capitalizes(str)
-
   str_split = str.split
-  str_capitalized = str_split.map{|word| word.capitalize()}
-  puts str_capitalized.join(" ")
+  str_capitalized = str_split.map{|word| word.capitalize}
+  str_capitalized.join(" ")
 
 end
 
