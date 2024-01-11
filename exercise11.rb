cbca068fdf8473c87a25a5d9fbd00b94673851d0
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-01-11 11:54:35 +0530

    Made the required changes

--- exercise11.rb
+++ exercise11.rb
@@ -1,6 +1,5 @@
 # 11.Create a method that returns the common elements between two arrays.
 
-
 def common_elements(arr1, arr2)
   common_elements_arr = []
 
@@ -8,13 +7,11 @@ def common_elements(arr1, arr2)
     common_elements_arr << element if arr2.include?(element)
   end
 
- return common_elements_arr
+ common_elements_arr
 end
-
 
 array1 = [1, 2, 3, 4, 5]
 array2 = [2, 3, 4, 5, 6, 7]
-
 result = common_elements(array1, array2)
 
 puts "Common Elements: #{result}"
