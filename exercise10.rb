73b475d00690e93edd1f14d175de3eebef534843
Kevinkp09 <kevinpatel907.kp@gmail.com>
2024-01-11 11:52:42 +0530

    Made the required changes

--- exercise10.rb
+++ exercise10.rb
@@ -2,22 +2,17 @@
 
 def arrays_equal(arr1, arr2)
 
-  return false unless arr1.length == arr2.length
-
-
   arr1.each_with_index do |element, i|
     return false unless element == arr2[i]
   end
 
-
   true
 end
-
 
 arr1 = [1, 2, 3, 4, 5]
 arr2 = [1, 2, 3, 4, 5]
 arr3 = [1, 2, 3, 4, 6]
-arr4 = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+arr4 = [1, 7, 3, 4, 5]
 
 puts arrays_equal(arr1, arr2)
 puts arrays_equal(arr1, arr3)
